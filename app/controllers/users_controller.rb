7d4599a91b56e7cb2ffad8fff0eed74d01b42e8a
alejandraaragon <alejita.0418@gmail.com>
2023-07-10 22:30:48 -0500

    segundo commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,57 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+
   def index
-    @users = User.all
+    @users = User.order("created_at DESC")
   end
 
   def new
     @user = User.new
   end
 
+  def show
+  end
+
+  def edit
+  end
+
   def create
     @user = User.new(user_params)
 
     if @user.save
-        redirect_to root_path, success: "User created successfully"
+      redirect_to root_path, success: "User created successfully"
     else
-        render :new, error: "Something went wrong"
+      flash.now[:error] = "Something went wrong"  # Modificación aquí
+      render :new
     end
   end
 
+  def update
+
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), success: "User was updated successfully"
+    else
+      render :edit, error: "Something went wrong"
+    end
+  end
+
+  def destroy
+    if @user.delete
+      redirect_to root_path, success: "User was deleted successfully"
+    else
+      render :show, error: "Something went wrong", status: :unprocessable_entity
+  end
+ end
+
     private
   def user_params
-    params.require(:user).permit(:fullname, :username, :age, :address, :email, :password_digest)
+    params.require(:user).permit(:fullname, :username, :age, :address, :email, :password_digest, :password_digest_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
- 
+   
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  
+  validates :fullname, :username, :address, :age, :email, :password_digest, presence: true
+  validates :password_digest, length: {minimum: 3, maximum: 10}
+  validates :password_digest, confirmation: true
+  validates :password_digest_confirmation, presence: true
 end
